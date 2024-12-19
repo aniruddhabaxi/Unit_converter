6f67a07abe265a30d318d7de54469b2ab2418fbf
Aniruddha <aniruddhabaxi@gmail.com>
2024-12-19 05:05:28 -0500

    files updates

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,30 @@
 //     let textValue = document.getElementById("text").value;
 //     document.getElementById("fetchText").innerText = textValue;
 //   }
-document.getElementById("text").addEventListener("input", function () {
-  let inputText = document.getElementById("text").value;
-  document.getElementById("fetchText").innerText = inputText;
-});
-document.getElementById("clickMe").addEventListener("click", function () {
-  document.getElementById("clickMe").style.backgroundColor = "#0aa9f6";
-  this.innerText = "Clicked..!";
-});
+// document.getElementById("text").addEventListener("input", function () {
+//   let inputText = document.getElementById("text").value;
+//   document.getElementById("fetchText").innerText = inputText;
+// });
+// document.getElementById("clickMe").addEventListener("click", function () {
+//   document.getElementById("clickMe").style.backgroundColor = "#0aa9f6";
+//   this.innerText = "Clicked..!";
+// });
+
+function weightConvert() {
+ const value = document.getElementById("inputVal").value;
+ const inputWeight = document.getElementById("fromWeight").value;
+ const outputWeight = document.getElementById("toWeight").value;
+
+  const unitBytes = {
+  'Gram': 1,
+  'Kilogram': 1000,
+  'Tonne': 1000 ** 2,
+  'Megatonne': 1000 ** 3,
+  'Gigatonne': 1000 ** 4
+  }
+
+  const unit_in_bytes = value * unitBytes[inputWeight];
+  const target_units = unit_in_bytes / unitBytes[outputWeight];
+
+  document.getElementById("outputResult").innerHTML = target_units + " " + outputWeight;
+}
